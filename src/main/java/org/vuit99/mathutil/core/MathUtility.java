a10fd9eaaa0968df07f5bdfc61fac64a358678b6
Vunt369 <ngtuanvu113@gmail.com>
2023-06-27 14:35:50 +0700

    update source using recursion; add screenshots to see green4ever

--- src/main/java/org/vuit99/mathutil/core/MathUtility.java
+++ src/main/java/org/vuit99/mathutil/core/MathUtility.java
@@ -8,7 +8,30 @@ package org.vuit99.mathutil.core;
 public class MathUtility {
 //class nay clone giong class java.util.Math 
  //chưa các hàm static Math.sqrt(), ..
-    public static final double PI = 3.14;
+//    public static final double PI = 3.14;
+//    
+//    //ham getF() la tinh m! = 1.2.3...n
+//    //0! = 1! = 1
+//    //ko co giai thua cho so am, neu dua -5! -> chui; nem ra Exception
+//    //vì giai th?a tang r?t nhanh, 21! long ko chua noi,(Long 18 so 0)
+//    //20! vua du cho long
+//    //21! 22! 23! > 20! > CHUI, nem ra Exception
+//    public static long getFactorial(int n){
+//        if(n<0 || n > 20){
+//            throw  new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
+//        }
+//        if(n == 0 || n == 1)
+//            return 1;
+//        long product = 1; //biến lưu dồn các tích
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        
+//        return product;
+//            
+//        
+//    }
+    
+        public static final double PI = 3.14;
     
     //ham getF() la tinh m! = 1.2.3...n
     //0! = 1! = 1
@@ -22,14 +45,9 @@ public class MathUtility {
         }
         if(n == 0 || n == 1)
             return 1;
-        long product = 1; //biến lưu dồn các tích
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        
-        return product;
-            
-        
+ 
+      return n * getFactorial(n-1);
     }
     
-    
+    //5! = 1.2.3.4.5 = n * (n-1)!
 }
